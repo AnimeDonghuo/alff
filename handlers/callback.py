d9b82b677d573268fade6ca0f3f23f07df912f52
AnimeDonghuo <165592897+AnimeDonghuo@users.noreply.github.com>
2026-07-21 00:13:49 +0530

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -1,10 +1,13 @@
 # handlers/callback.py
-from pyrogram import Client, filters
+from pyrogram import Client
+from pyrogram.handlers import CallbackQueryHandler
 from pyrogram.types import CallbackQuery
 from utils.logger import logger
 
-@Client.on_callback_query()
 async def main_callback_handler(client: Client, callback_query: CallbackQuery):
     data = callback_query.data
-    logger.info(f"Received callback payload: {data}")
+    logger.info(f"[UPDATE] Received callback payload: {data}")
     await callback_query.answer("Payload registered.", show_alert=False)
+
+def register_callback_handlers(app: Client):
+    app.add_handler(CallbackQueryHandler(main_callback_handler))
