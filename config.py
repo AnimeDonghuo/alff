9fe2298037f542942337fe10ef798b33e06f9296
AnimeDonghuo <165592897+AnimeDonghuo@users.noreply.github.com>
2026-08-08 13:29:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
-API_ID: int = int(os.getenv("API_ID", ""))
+API_ID: int = int(os.getenv("API_ID", "0"))
 API_HASH: str = os.getenv("API_HASH", "")
 OWNER_ID: int = int(os.getenv("OWNER_ID", "1685470205"))
 DEFAULT_CHECK_INTERVAL: int = int(os.getenv("CHECK_INTERVAL", "15"))
-DATABASE_PATH: str = "bot.db"
+MONGO_URI: str = os.getenv("MONGO_URI", "")
